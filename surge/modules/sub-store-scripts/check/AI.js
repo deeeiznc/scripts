da7390d5c2f459ab5724b5d8f5d5c60922a6546b
deeeiznc <68291672+deeeiznc@users.noreply.github.com>
2024-12-20 01:39:46 +0800

    Add files via upload

--- surge/modules/sub-store-scripts/check/AI.js
+++ surge/modules/sub-store-scripts/check/AI.js
@@ -27,7 +27,7 @@ async function operator(proxies = [], targetPlatform, context) {
   const method = $arguments.method || 'get'
   const gpt_url = $arguments.client === 'Android' ? `https://android.chat.openai.com` : `https://ios.chat.openai.com`
   const aistudio_url = `https://aistudio.google.com`
-    const claude_url = `https://claude.ai`
+  const claude_url = `https://claude.ai`
   const target = isLoon ? 'Loon' : isSurge ? 'Surge' : undefined
   const concurrency = parseInt($arguments.concurrency || 10) // 一组并发数
   await executeAsyncTasks(
@@ -38,21 +38,21 @@ async function operator(proxies = [], targetPlatform, context) {
 
   return proxies
 
-    async function check(proxy) {
-        const hkRegex = /🇭🇰|香港|Hong|HK/i
-        if (!hkRegex.test(proxy.name)) {
-            return
-          }
-        // $.info(`[${proxy.name}] 检测`)
-        // $.info(`检测 ${JSON.stringify(proxy, null, 2)}`)
-        const id = cacheEnabled
-            ? `gpt:${gpt_url}:${aistudio_url}:${claude_url}:${JSON.stringify(
-                Object.fromEntries(
-                    Object.entries(proxy).filter(([key]) => !/^(name|collectionName|subName|id|_.*)$/i.test(key))
-                )
-            )}`
-            : undefined
-        // $.info(`检测 ${id}`)
+  async function check(proxy) {
+    const hkRegex = /🇭🇰|香港|Hong|HK/i
+    if (!hkRegex.test(proxy.name)) {
+      return
+    }
+    // $.info(`[${proxy.name}] 检测`)
+    // $.info(`检测 ${JSON.stringify(proxy, null, 2)}`)
+    const id = cacheEnabled
+      ? `gpt:${gpt_url}:${aistudio_url}:${claude_url}:${JSON.stringify(
+        Object.fromEntries(
+          Object.entries(proxy).filter(([key]) => !/^(name|collectionName|subName|id|_.*)$/i.test(key))
+        )
+      )}`
+      : undefined
+    // $.info(`检测 ${id}`)
     try {
       const node = ProxyUtils.produce([proxy], target)
       if (node) {
@@ -65,114 +65,114 @@ async function operator(proxies = [], targetPlatform, context) {
           return
         }
         // 请求
-          const gpt_status = await checkGpt(proxy, node);
-          const aistudio_status = await checkAistudio(proxy, node);
-          const claude_status = await checkClaude(proxy, node);
+        const gpt_status = await checkGpt(proxy, node);
+        const aistudio_status = await checkAistudio(proxy, node);
+        const claude_status = await checkClaude(proxy, node);
         if (gpt_status && aistudio_status && claude_status) {
           proxy.name = `[AI] ${proxy.name}`
           if (cacheEnabled) {
-               $.info(`[${proxy.name}] 设置成功缓存`)
+            $.info(`[${proxy.name}] 设置成功缓存`)
             cache.set(id, { gpt: true })
           }
         } else {
-              if (cacheEnabled) {
-                $.info(`[${proxy.name}] 设置失败缓存`)
-                  cache.set(id, { })
-            }
+          if (cacheEnabled) {
+            $.info(`[${proxy.name}] 设置失败缓存`)
+            cache.set(id, {})
+          }
         }
       }
     } catch (e) {
       $.error(`[${proxy.name}] ${e.message ?? e}`)
-        if (cacheEnabled) {
-            $.info(`[${proxy.name}] 设置失败缓存`)
-            cache.set(id, {})
-        }
+      if (cacheEnabled) {
+        $.info(`[${proxy.name}] 设置失败缓存`)
+        cache.set(id, {})
+      }
     }
   }
   async function checkGpt(proxy, node) {
-      const startedAt = Date.now();
-        const res = await http({
-            method,
-            headers: {
-                'User-Agent':
-                    'Mozilla/5.0 (iPhone; CPU iPhone OS 17_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.3.1 Mobile/15E148 Safari/604.1',
-            },
-            url: gpt_url,
-            'policy-descriptor': node,
-            node,
-        });
-        const status = parseInt(res.status ?? res.statusCode ?? 200);
-        let body = String(res.body ?? res.rawBody);
-        try {
-            body = JSON.parse(body);
-        } catch (e) {}
-        // $.info(`body ${JSON.stringify(body, null, 2)}`)
-        const msg = body?.error?.error_type || body?.cf_details;
-        let latency = `${Date.now() - startedAt}`;
-      $.info(`[${proxy.name}] GPT status: ${status}, msg: ${msg}, latency: ${latency}`);
-        // cf 拦截是 400 错误, 403 就是没被拦截, 走到了未鉴权的逻辑
-        // https://zset.cc/archives/34/
-        // 更新: 403 的时候, 还得看响应
-        if (status == 403 && !['unsupported_country'].includes(msg)) {
-        return true;
-          
-        } else {
-         return false;
-        }
-      
+    const startedAt = Date.now();
+    const res = await http({
+      method,
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (iPhone; CPU iPhone OS 17_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.3.1 Mobile/15E148 Safari/604.1',
+      },
+      url: gpt_url,
+      'policy-descriptor': node,
+      node,
+    });
+    const status = parseInt(res.status ?? res.statusCode ?? 200);
+    let body = String(res.body ?? res.rawBody);
+    try {
+      body = JSON.parse(body);
+    } catch (e) { }
+    // $.info(`body ${JSON.stringify(body, null, 2)}`)
+    const msg = body?.error?.error_type || body?.cf_details;
+    let latency = `${Date.now() - startedAt}`;
+    $.info(`[${proxy.name}] GPT status: ${status}, msg: ${msg}, latency: ${latency}`);
+    // cf 拦截是 400 错误, 403 就是没被拦截, 走到了未鉴权的逻辑
+    // https://zset.cc/archives/34/
+    // 更新: 403 的时候, 还得看响应
+    if (status == 403 && !['unsupported_country'].includes(msg)) {
+      return true;
+
+    } else {
+      return false;
+    }
+
   }
   async function checkAistudio(proxy, node) {
-         const startedAt = Date.now()
-      const res = await http({
-        method,
-        headers: {
-          'User-Agent':
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
-        },
-        url: aistudio_url,
-        'policy-descriptor': node,
-        node,
-      })
-           const status = parseInt(res.status ?? res.statusCode ?? 200);
-          
-         let body = String(res.body ?? res.rawBody)
-           let latency = `${Date.now() - startedAt}`
-              $.info(`[${proxy.name}] Aistudio status: ${status}, latency: ${latency}`)
-          
-   
-        if (status >= 200 && status < 400 ) {
-            
-          return true;
-        } else {
-       return false
-       }
+    const startedAt = Date.now()
+    const res = await http({
+      method,
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+      },
+      url: aistudio_url,
+      'policy-descriptor': node,
+      node,
+    })
+    const status = parseInt(res.status ?? res.statusCode ?? 200);
+
+    let body = String(res.body ?? res.rawBody)
+    let latency = `${Date.now() - startedAt}`
+    $.info(`[${proxy.name}] Aistudio status: ${status}, latency: ${latency}`)
+
+
+    if (status >= 200 && status < 400) {
+
+      return true;
+    } else {
+      return false
+    }
   }
- async function checkClaude(proxy, node) {
-      const startedAt = Date.now();
-   const res = await http({
-        method,
-        headers: {
-          'User-Agent':
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
-        },
-        url: claude_url,
-        'policy-descriptor': node,
-        node,
-      })
-      const status = parseInt(res.status ?? res.statusCode ?? 200);
-    
-       let body = String(res.body ?? res.rawBody)
-       let latency = `${Date.now() - startedAt}`
-        $.info(`[${proxy.name}] Claude status: ${status}, latency: ${latency}`)
-        if (status >= 200 && status < 400 ) {
-            
-             if(body.includes("unavailable-in-region")){
-                  return false
-             }
-          return true;
-        } else {
-           return false
-        }
+  async function checkClaude(proxy, node) {
+    const startedAt = Date.now();
+    const res = await http({
+      method,
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+      },
+      url: claude_url,
+      'policy-descriptor': node,
+      node,
+    })
+    const status = parseInt(res.status ?? res.statusCode ?? 200);
+
+    let body = String(res.body ?? res.rawBody)
+    let latency = `${Date.now() - startedAt}`
+    $.info(`[${proxy.name}] Claude status: ${status}, latency: ${latency}`)
+    if (status >= 200 && status < 400) {
+
+      if (body.includes("unavailable-in-region")) {
+        return false
+      }
+      return true;
+    } else {
+      return false
+    }
   }
   // 请求
   async function http(opt = {}) {
